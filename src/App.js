2069472c5dcc03b532c0642563ce4aa52b0cb0e8
zioposty <l.postiglione4@studenti.unisa.it>
2022-09-14 14:12:38 +0200

    first version of React client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Graph from './components/Graph';
+import SingleChart from './components/SingleChart';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import { Route, Routes} from 'react-router-dom'
 
 function App() {
+  let socket = new WebSocket("ws://127.0.0.1:3012");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor:"#211f1a"}}>
+      <ResponsiveAppBar />
+      <div className='content'>
+        <Routes>
+          <Route exact path="/" element={ <Graph socket={socket} /> } />
+          <Route path="/Chart/:filename" element={ <SingleChart socket={socket} /> } />
+        </Routes>
+      </div>
     </div>
   );
 }
